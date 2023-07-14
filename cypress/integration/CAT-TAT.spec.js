6141efb3e436865506a925fc84f5bfb77268b16c
Janafalcao84 <jan.falcao@hotmail.com>
2023-07-14 19:53:28 -0300

    Commit cypress project

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,12 +1,20 @@
-
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-      cy.visit('./src/index.html')
-
-      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
-    })
+  beforeEach(function() {
+    cy.visit('./src/index.html')
   })
+  it('verifica o título da aplicação', function() {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')  
+  })
+  it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    cy.get('#firstName').type('Janaina')
+    cy.get('#lastName').type('Falcão')
+    cy.get('#email').type('janaina@exemplo.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+})
   
